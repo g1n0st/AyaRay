948685bee968762416f161f9c4b8960db2adafb7
g1n0st <g1n0st@live.com>
2019-12-10 16:37:25 +0800

    Fixed randomInUnitSphere bug

--- src/core/rng.h
+++ src/core/rng.h
@@ -41,7 +41,7 @@ public:
 	Point3 randomInUnitSphere() {
 		Point3 p;
 		do {
-			p = (2 * Point3(drand48(), drand48(), drand48()) - Point3(1.f, 1.f, 0.f)) * 2.f;
+			p = 2 * Point3(drand48(), drand48(), drand48()) - Point3(1.f, 1.f, 1.f);
 		} while (p.length2() >= 1.f);
 		return p;
 	}
