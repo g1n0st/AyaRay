8c5056a9a89ba2442509950d302ff854aa2db95d
g1n0st <g1n0st@live.com>
2019-12-11 15:45:17 +0800

    Temporary integrator

--- src/core/integrator.h
+++ src/core/integrator.h
@@ -3,45 +3,45 @@
 
 #include "config.h"
 #include "camera.h"
-#include "material.h"
+#include "primitive.h"
 
-class Integrator {
-public:
-	virtual Spectrum li(const Ray &ray, Shape **shapes, int shape_size, int depth) const = 0;
-};
+namespace Aya {
+	class Integrator {
+	public:
+		virtual Spectrum li(const Ray &ray, GeometricPrimitive **prim, int shape_size, int depth) const = 0;
+	};
 
-class SampleIntegrator : Integrator {
-public:
-	virtual Spectrum li(const Ray &ray, Shape **shapes, int shape_size, int depth) const {
-		SurfaceInteraction si;
-		float hit;
-		bool hit_object = false;
-		for (int i = 0; i < shape_size; i++) {
-			if (shapes[i]->intersect(ray, &hit, &si)) {
-				hit_object = true;
-				ray.m_maxt = hit;
+	class SampleIntegrator : Integrator {
+	public:
+		virtual Spectrum li(const Ray &ray, GeometricPrimitive **prim, int shape_size, int depth) const {
+			SurfaceInteraction si;
+			bool hit_object = false;
+			for (int i = 0; i < shape_size; i++) {
+				if (prim[i]->intersect(ray, &si)) {
+					hit_object = true;
+				}
 			}
-		}
-		if (hit_object) {
-			Ray scatter;
-			Spectrum attenuation;
+			if (hit_object) {
+				Ray scatter;
+				Spectrum attenuation;
 
-			Spectrum emitted = si.mat->emitted(si.u, si.v, si.p);
-			if (depth < 50 && si.mat->scatter(ray, si, attenuation, scatter)) {
-				return (emitted + attenuation * li(scatter, shapes, shape_size, depth + 1)).clamp(0.f, 1.f);
+				Spectrum emitted = si.prim->m_material->emitted(si.u, si.v, si.p);
+				if (depth < 50 && si.prim->m_material->scatter(ray, si, attenuation, scatter)) {
+					return (emitted + attenuation * li(scatter, prim, shape_size, depth + 1)).clamp(0.f, 1.f);
+				}
+				else {
+					return emitted;
+				}
 			}
 			else {
-				return emitted;
+				// sky box
+				Vector3 ud = ray.m_dir;
+				ud.normalize();
+
+				float t = .5f * (ud.y() + 1.f);
+				return ((1.f - t) * Spectrum(.5f, .5f, .5f) + t * Spectrum(.5f, .7f, 1.f)).clamp(0.0f, 1.0f);
 			}
 		}
-		else {
-			// sky box
-			Vector3 ud = ray.m_dir;
-			ud.normalize();
-
-			float t = .5f * (ud.y() + 1.f);
-			return ((1.f - t) * Spectrum(.1f, .1f, .1f) + t * Spectrum(.5f, .7f, 1.f)).clamp(0.0f, 1.0f);
-		}
-	}
-};
+	};
+}
 #endif
