8fb4d7be8396a0024e535cbfbe34982c60595d63
g1n0st <g1n0st@live.com>
2019-12-23 23:17:04 +0800

    Update main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -7,7 +7,7 @@
 #include "../shapes/sphere.h"
 #include "../shapes/triangle.h"
 #include "../accelerators/BVH.h"
-
+#include "parser.h"
 
 
 #include <iostream>
@@ -147,11 +147,11 @@ int vvv[] = {
 
 int main() {
 
+	/*
 	readFromSimFile();
 
 	int nx = 200, ny = 200;
 	int sample_times = 500;
-
 	Aya::Transform t0 = Aya::Transform().setTranslate(5, 0, 0);
 	//* Aya::Transform().setEulerZYX(-20, 90, 90);
 	Aya::Transform _t0 = t0.inverse();
@@ -164,22 +164,10 @@ int main() {
 	//* Aya::Transform().setEulerZYX(-20, 90, 90);
 	Aya::Transform _t11 = t11.inverse();
 
-
-	Aya::Transform t2 = Aya::Transform().setTranslate(7, 3, 0);
-	Aya::Transform _t2 = t2.inverse();
-
-	Aya::Transform t3 = Aya::Transform().setTranslate(5, 0, 0);
-	Aya::Transform _t3 = t3.inverse();
-
-	Aya::Transform t4 = Aya::Transform().setTranslate(0, -10, 0);
-	Aya::Transform _t4 = t4.inverse();
-
 	//Aya::SharedPtr<Aya::Shape> shape0(new Aya::TriangleMesh(&t0, &_t0, 1, 4, vvv, ppp, nnn, NULL));
 	Aya::SharedPtr<Aya::Shape> shape0(new Aya::TriangleMesh(&t0, &_t0, TS, VS, VV, PP, NULL, NULL));
 	Aya::SharedPtr<Aya::Shape> shape1(new Aya::Rectangle(&t1, &_t1, 1, 1, 1));
-	Aya::SharedPtr<Aya::Shape> dub(new Aya::Sphere(&t2, &_t2, 0.7f));
 	Aya::SharedPtr<Aya::Shape> light(new Aya::Sphere(&t11, &_t11, 1.f));
-	Aya::SharedPtr<Aya::Shape> box(new Aya::Rectangle(&t4, &_t4, 400.0f, 18.5f, 400.0f));
 
 	Aya::SharedPtr<Aya::Material> mental = new Aya::MentalMaterial(Aya::Spectrum(191.0 / 256.0, 173.0 / 256.0, 111.0 / 256.0), 0.0f);
 	Aya::SharedPtr<Aya::Material> mat = new Aya::LambertianMaterial(new Aya::ConstantTexture(Aya::Spectrum(0.3, 0.3, 0.3)));
@@ -207,6 +195,12 @@ int main() {
 	scene.render("output.ppm");
 	//Aya::ProjectiveCamera cam(Aya::Point3(278, 273, -800), Aya::Vector3(0, 0, 1), Aya::Vector3(0, 1, 0), 45.f, float(nx) / float(ny), 0.0f, 2.f, 0.f, 0.f);
 	
+	*/
 
+	Aya::Scene *scene = new Aya::Scene();
+	Aya::Parser parser;
+	parser.load("test.json");
+	parser.run(scene);
+	scene->render("parser.ppm");
 	return 0;
 }
