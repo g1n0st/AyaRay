c2a2c852336330e104adcf03fb58eb1d4c00d67e
g1n0st <g1n0st@live.com>
2019-12-27 01:28:39 +0800

    Update scene.h

--- src/core/scene.h
+++ src/core/scene.h
@@ -9,19 +9,28 @@
 #include <fstream>
 
 namespace Aya {
+	/**@brief Scene class defines everything needed for a renderable scene */
 	class Scene {
 	private:
 		RNG rng;
 	public:
-		int m_screen_x, m_screen_y, m_sample_times;
+		/**@brief Render the picture's length and width pixels */
+		int m_screen_x, m_screen_y;
+		/**@brief Number of samples per pixel */
+		int m_sample_times;
+		/**@brief Accelerator used by the scene */
 		SharedPtr<Accelerator> m_acc;
+		/**@brief Camera used by the scene */
 		SharedPtr<Camera> m_cam;
+		/**@brief Integrator used by the scene */
 		SharedPtr<Integrator> m_int;
 
 	public:
 		Scene() {}
 		Scene(const int &x, const int &y, const int &time);
 
+		/**@brief start rendering
+		* @param output output file name */
 		void render(const char *output);
 	};
 }
