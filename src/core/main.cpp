d0c89dff39706bccf46da22249bc75db988e42ff
g1n0st <g1n0st@live.com>
2020-01-04 10:11:12 +0800

    Update main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,11 +1,15 @@
 #include "scene.h"
 #include "parser.h"
 
-int main() {
+int main(int argc, char **argv) {
+	if (argc != 3) {
+		printf("the argv is error.\n");
+		exit(1);
+	}	
 	Aya::Scene *scene = new Aya::Scene();
 	Aya::Parser parser;
-	parser.load("test.json");
+	parser.load(argv[1]);
 	parser.run(scene);
-	scene->render("parser.ppm");
+	scene->render(argv[2]);
 	return 0;
 }
