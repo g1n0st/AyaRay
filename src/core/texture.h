2f0f707358cd73fc399f4b5c19e55a7b4406941c
g1n0st <g1n0st@live.com>
2020-01-04 10:11:20 +0800

    Update texture.h

--- src/core/texture.h
+++ src/core/texture.h
@@ -48,9 +48,9 @@ namespace Aya {
 		CrossTexture(Texture *t0, Texture *t1) : m_t0(t0), m_t1(t1) {}
 
 		virtual Spectrum value(float u, float v, const Point3 &p) const {
-			float sines = sinf(10 * p.x()) *
-				sinf(10 * p.y()) *
-				sinf(10 * p.z());
+			float sines = sinf(0.1f * p.x()) *
+				sinf(0.1f * p.y()) *
+				sinf(0.1f * p.z());
 			return sines < 0 ? m_t0->value(u, v, p) : m_t1->value(u, v, p);
 		}
 	};
