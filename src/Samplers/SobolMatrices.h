13fdaf5ad40cc3dad8ba4526ec42f71a606d5d5d
g1n0st <g1n0st@live.com>
2020-02-08 21:49:12 +0800

    Add Sobol sampler required data

---
+++ src/Samplers/SobolMatrices.h
@@ -0,0 +1,17 @@
+#ifndef AYA_SAMPLER_SOBOLMATRICES_H
+#define AYA_SAMPLER_SOBOLMATRICES_H
+
+#include "../Core/Config.h"
+
+namespace Aya {
+		// data source: https://web.maths.unsw.edu.au/~fkuo/sobol/
+		// Sobol Matrix Declarations
+		static const int num_sobol_dimensions = 1024;
+		static const int sobol_matrix_size = 52;
+		extern const uint32_t sobol_matrices32[num_sobol_dimensions * sobol_matrix_size];
+		extern const uint64_t sobol_matrices64[num_sobol_dimensions * sobol_matrix_size];
+		extern const uint64_t VdC_sobol_matrices[][sobol_matrix_size];
+		extern const uint64_t VdC_sobol_matrices_inv[][sobol_matrix_size];
+}
+
+#endif
