3f243b093ee9b9e77b4d6187415f15ab5ea125a2
g1n0st <g1n0st@live.com>
2020-02-19 20:51:43 +0800

    Created Mirror BSDF class declaration

---
+++ src/BSDFs/Mirror.h
@@ -0,0 +1,29 @@
+#ifndef AYA_BSDFS_MIRROR_H
+#define AYA_BSDFS_MIRROR_H
+
+#include "../Core/BSDF.h"
+
+namespace Aya {
+	class Mirror : public BSDF {
+	public:
+		Mirror(const Spectrum &color = RGBSpectrum().toSpectrum())
+			: BSDF(ScatterType(BSDF_REFLECTION | BSDF_SPECULAR), BSDFType::Mirror, color) {}
+		Mirror(UniquePtr<Texture2D<Spectrum>> tex, UniquePtr<Texture2D<Spectrum>> normal)
+			: BSDF(ScatterType(BSDF_REFLECTION | BSDF_SPECULAR), BSDFType::Mirror, std::move(tex), std::move(normal)) {}
+		Mirror(const char* texture_file)
+			: BSDF(ScatterType(BSDF_REFLECTION | BSDF_SPECULAR), BSDFType::Mirror, texture_file) {}
+		Mirror(const char* texture_file, const char* normal_file)
+			: BSDF(ScatterType(BSDF_REFLECTION | BSDF_SPECULAR), BSDFType::Mirror, texture_file, normal_file) {}
+
+		virtual Spectrum f(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types = BSDF_ALL) const override;
+		virtual float pdf(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types = BSDF_ALL) const override;
+		virtual Spectrum sample_f(const Vector3 &v_out, const Sample &sample,
+			const SurfaceIntersection &intersection, Vector3* v_in, float* pdf, ScatterType types = BSDF_ALL, ScatterType *sample_types = nullptr) const override;
+
+	private:
+		virtual float evalInner(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types = BSDF_ALL) const override;
+		virtual float pdfInner(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types = BSDF_ALL) const override;
+	};
+}
+
+#endif
