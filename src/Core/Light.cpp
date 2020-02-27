2d2f0e0b08d900b84539a7d8ff2df22263891321
g1n0st <g1n0st@live.com>
2020-02-27 23:25:13 +0800

    Added Light base interface class

---
+++ src/Core/Light.cpp
@@ -0,0 +1,13 @@
+#include "Light.h"
+
+namespace Aya {
+	bool VisibilityTester::unoccluded(const Scene * scene) const {
+		return !scene->occluded(ray);
+	}
+	Spectrum VisibilityTester::transmittance(const Scene * scene, Sampler * sampler) const {
+		if (ray.mp_medium)
+			return ray.mp_medium->tr(ray, sampler);
+		else
+			return Spectrum::fromRGB(1.f, 1.f, 1.f);
+	}
+}
--- src/Core/Light.h
+++ src/Core/Light.h
@@ -3,12 +3,69 @@
 
 #include "../Core/Ray.h"
 #include "../Math/Vector3.h"
+#include "../Core/Scene.h"
+#include "../Core/Intersection.h"
+#include "../Core/Sampler.h"
 
 namespace Aya {
 	class VisibilityTester {
 	private:
-		Ray m_ray;
+		Ray ray;
 
+	public:
+		void setSegment(const Point3 &pt1, const Point3 &pt2) {
+			float dist = pt1.distance(pt2);
+			ray = Ray(pt1, (pt2 - pt1) / dist, nullptr, 0.f, dist);
+		}
+		void setRay(const Point3 &pt, const Vector3 &dir, const float max_length = INFINITY - AYA_RAY_EPS) {
+			ray = Ray(pt, dir, nullptr, 0.f, max_length);
+		}
+		void setMedium(const Medium *medium) {
+			ray.mp_medium = medium;
+		}
+
+		bool unoccluded(const Scene *scene) const;
+		Spectrum transmittance(const Scene *scene, Sampler *sampler) const;
+	};
+
+	class Light {
+	protected:
+		uint32_t m_sample_count;
+
+	public:
+		Light(uint32_t count) :
+			m_sample_count(count) {}
+
+		virtual ~Light() {}
+		virtual Spectrum illuminate(const Scatter &scatter,
+			const Sample &light_sample,
+			Vector3 *dir,
+			VisibilityTester *tester,
+			float *pdf,
+			float *cos_at_light = nullptr,
+			float *emit_pdf_w = nullptr) const = 0;
+		virtual Spectrum sample(const Sample &light_sample0,
+			const Sample &light_sample1,
+			Ray *ray,
+			Normal3 *normal,
+			float *pdf,
+			float *direct_pdf = nullptr) const = 0;
+		virtual Spectrum emit(const Vector3 &dir,
+			const Normal3 &normal = Normal3(0.f, 0.f, 0.f),
+			float *pdf = nullptr,
+			float *direct_pdf = nullptr) const = 0;
+		virtual float pdf(const Point3 &pos, const Vector3 &dir) const = 0;
+		virtual bool isEnvironmentLight() const {
+			return false;
+		}
+		virtual bool isAreaLight() const {
+			return false;
+		}
+		virtual bool isDelta() const = 0;
+		virtual bool isFinite() const = 0;
+		uint32_t getSampleCount() const {
+			return m_sample_count;
+		}
 	};
 }
 
