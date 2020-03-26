5f502ce4713464b3f33186c6f7362f98634bf9d0
g1n0st <g1n0st@live.com>
2020-03-26 19:37:25 +0800

    Support Disney BRDF (passed unit test)

--- src/BSDFs/Disney.cpp
+++ src/BSDFs/Disney.cpp
@@ -1,21 +1,169 @@
 #include "Disney.h"
 
 namespace Aya {
-	float Disney::pdfInner(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types) const {
-		Vector3 wh = (v_out + v_in).normalize();
+	float Disney::pdfInner(const Vector3 &l_out, const Vector3 &l_in, const SurfaceIntersection &intersection, ScatterType types) const {
+		Vector3 wh = (l_out + l_in).normalize();
 		if (wh.length2() == 0.f)
 			return 0.f;
 
-		float roughness = getValue(m_roughness.get(), intersection, TextureFilter::Linear);
+		float roughness = getValue(mp_roughness.get(), intersection, TextureFilter::Linear);
 		roughness = Clamp(roughness, .02f, 1.f);
 
-		float microfacet_pdf = GGX_Pdf_VisibleNormal(v_out, wh, roughness * roughness);
+		float microfacet_pdf = GGX_Pdf_VisibleNormal(l_out, wh, roughness * roughness);
 		float pdf = 0.f;
-		float dwh_dwi = 1.f / (4.f * Abs(v_in.dot(wh)));
+		float dwh_dwi = 1.f / (4.f * Abs(l_in.dot(wh)));
 		float spec_pdf = microfacet_pdf * dwh_dwi;
 
-		float norm_ref = Lerp(m_specular, .0f, .08f);
-		float odh = v_out.dot(wh);
-		float prob_spec = Fresnel_
+		float normal_ref = Lerp(m_specular, .0f, .08f);
+		float OdotH = l_out.dot(wh);
+		float prob_spec = Fresnel_Schlick(OdotH, normal_ref);
+
+		pdf += spec_pdf * prob_spec;
+		pdf += AbsCosTheta(l_in) * float(M_1_PI) * (1.f - prob_spec);
+
+		if (m_clear_coat > 0.f) {
+			Spectrum albedo = getValue(mp_texture.get(), intersection);
+			float coat_weight = m_clear_coat / (m_clear_coat + albedo.y());
+			float fresnel_coat = Fresnel_Schlick_Coat(AbsCosTheta(l_out));
+			float prob_coat = (fresnel_coat * coat_weight) /
+				(fresnel_coat * coat_weight +
+				(1.f - fresnel_coat) * (1.f - coat_weight));
+			float coat_rough = Lerp(m_clear_coat_gloss, .005f, .10f);
+			float coat_half_pdf = GGX_Pdf_VisibleNormal(l_out, wh, coat_rough);
+			float coat_pdf = coat_half_pdf * dwh_dwi;
+
+			pdf *= 1.f - prob_coat;
+			pdf += coat_pdf * prob_coat;
+		}
+
+		return pdf;
+	}
+
+	float Disney::evalInner(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types) const {
+		return 0.f;
+	}
+
+	Spectrum Disney::f(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types) const {
+		if (v_out.dot(intersection.gn) * v_in.dot(intersection.gn) > 0.f)
+			types = ScatterType(types & ~BSDF_TRANSMISSION);
+		else
+			types = ScatterType(types & ~BSDF_REFLECTION);
+
+		if (!matchesTypes(types))
+			return Spectrum(0.f);
+
+		Vector3 l_out = intersection.worldToLocal(v_out);
+		Vector3 l_in = intersection.worldToLocal(v_in);
+
+		return evaluate(l_out, l_in, intersection, types);
+	}
+
+	Spectrum Disney::evaluate(const Vector3 &l_out, const Vector3 &l_in, const SurfaceIntersection &intersection, ScatterType types) const {
+		Spectrum albedo = getValue(mp_texture.get(), intersection);
+		float roughness = getValue(mp_roughness.get(), intersection, TextureFilter::Linear);
+		roughness = Clamp(roughness, .02f, 1.f);
+
+		Spectrum Ctint = albedo.y(); // luminance approx.
+		Spectrum Cspec0 = Lerp(m_metallic, // dielectric -> metallic
+			Lerp(1.f - m_specular_tint, albedo, Ctint), // baseColor -> Colorless
+			albedo);
+		Spectrum Csheen = Lerp(m_sheen_tint, Ctint, albedo); // Colorless -> baseColor
+
+		Vector3 wh = (l_out + l_in).normalize();
+		float OdotH = l_out.dot(wh);
+		float IdotH = l_in.dot(wh);
+		float _1_OdotH = 1.f - OdotH;
+		Cspec0 = Lerp(_1_OdotH * _1_OdotH * _1_OdotH, Cspec0, Ctint);
+
+		// sheen
+		float FH = Fresnel_Schlick(OdotH, 0.f);
+		Spectrum sheen = FH * m_sheen * Csheen;
+
+		return (1.f - m_metallic)
+			* (albedo * Lerp(m_subsurface, diffuseTerm(l_out, l_in, IdotH, roughness), subsurfaceTerm(l_out, l_in, IdotH, roughness))
+				+ sheen)
+			+ Cspec0 * specularTerm(l_out, l_in, wh, OdotH, roughness, nullptr)
+			+ Spectrum(clearCoatTerm(l_out, l_in, wh, IdotH, m_clear_coat_gloss));
+	}
+
+	Spectrum Disney::sample_f(const Vector3 &v_out, const Sample &sample, const SurfaceIntersection &intersection, 
+		Vector3 *v_in, float *pdf, ScatterType types, ScatterType *sample_types) const {
+		if (!matchesTypes(types)) {
+			*pdf = 0.f;
+			return Spectrum(0.f);
+		}
+
+		const Vector3 l_out = intersection.worldToLocal(v_out);
+		Vector3 l_in, wh;
+		bool sample_coat = false;
+		Sample remapped_sample = sample;
+
+		if (m_clear_coat > 0.f) {
+			Spectrum albedo = getValue(mp_texture.get(), intersection);
+			float coat_weight = m_clear_coat / (m_clear_coat + albedo.y());
+			float fresnel_coat = Fresnel_Schlick_Coat(AbsCosTheta(l_out));
+			float prob_coat = (fresnel_coat * coat_weight) /
+				(fresnel_coat * coat_weight +
+				(1.f - fresnel_coat) * (1.f - coat_weight));
+
+			if (sample.v < prob_coat) {
+				sample_coat = true;
+				remapped_sample.v /= prob_coat;
+
+				float coat_rough = Lerp(m_clear_coat_gloss, .005f, .1f);
+				float coat_pdf;
+				wh = GGX_SampleVisibleNormal(l_out, remapped_sample.u, remapped_sample.v, &coat_pdf, coat_rough);
+				l_in = -l_out + 2.f * l_out.dot(wh) * wh;
+				*sample_types = ScatterType(BSDF_REFLECTION | BSDF_GLOSSY);
+			}
+			else {
+				sample_coat = false;
+				remapped_sample.v = (sample.v - prob_coat) / (1.f - prob_coat);
+			}
+		}
+
+		if (!sample_coat) {
+			float microfacet_pdf;
+			float roughness = getValue(mp_roughness.get(), intersection, TextureFilter::Linear);
+			roughness = Clamp(roughness, .02f, 1.f);
+
+			wh = GGX_SampleVisibleNormal(l_out, remapped_sample.u, remapped_sample.v, &microfacet_pdf, roughness * roughness);
+
+			float normal_ref = Lerp(m_specular, .0f, .08f);
+			float OdotH = l_out.dot(wh);
+			float prob_spec = Fresnel_Schlick(OdotH, normal_ref);
+
+			if (remapped_sample.w <= prob_spec) {
+				l_in = -l_out + 2.f * l_out.dot(wh) * wh;
+				*sample_types = ScatterType(BSDF_REFLECTION | BSDF_GLOSSY);
+			}
+			else {
+				l_in = CosineSampleHemisphere(remapped_sample.u, remapped_sample.v);
+				if (l_out.z() < 0.f)
+					l_in.setZ(l_in.z() * -1.f);
+
+				*sample_types = ScatterType(BSDF_REFLECTION | BSDF_DIFFUSE);
+			}
+		}
+
+		if (l_in.z() <= 0.0f) {
+			*pdf = 0.f;
+			return Spectrum(0.f);
+		}
+
+		*v_in = intersection.localToWorld(l_in);
+		*pdf = pdfInner(l_out, l_in, intersection, types);
+
+		if (v_out.dot(intersection.gn) * v_in->dot(intersection.gn) > 0.f)
+			types = ScatterType(types & ~BSDF_TRANSMISSION);
+		else
+			types = ScatterType(types & ~BSDF_REFLECTION);
+
+		if (!matchesTypes(types)) {
+			*pdf = 0.f;
+			return Spectrum(0.f);
+		}
+
+		return evaluate(l_out, l_in, intersection, types);
 	}
 }
--- src/BSDFs/Disney.h
+++ src/BSDFs/Disney.h
@@ -6,7 +6,7 @@
 namespace Aya {
 	class Disney : public BSDF {
 	private:
-		UniquePtr<Texture2D<float>> m_roughness;
+		UniquePtr<Texture2D<float>> mp_roughness;
 		float m_specular;
 		float m_metallic;
 		float m_specular_tint;
@@ -28,7 +28,7 @@ namespace Aya {
 			float clear_coat					= 0.0f,
 			float clear_coat_gloss			= 0.0f
 		) : BSDF(ScatterType(BSDF_REFLECTION | BSDF_DIFFUSE | BSDF_GLOSSY), BSDFType::Disney, reflectance),
-			m_roughness(new ConstantTexture2D<float>(roughness)),
+			mp_roughness(new ConstantTexture2D<float>(roughness)),
 			m_specular(specular),
 			m_metallic(metallic),
 			m_specular_tint(specular_tint),
@@ -40,17 +40,17 @@ namespace Aya {
 
 		Disney(UniquePtr<Texture2D<Spectrum>> texture,
 			UniquePtr<Texture2D<Spectrum>> normal,
-			float roughness = 0.1f,
-			float specular = 0.5f,
-			float metallic = 0.0f,
-			float specular_tint = 0.0f,
-			float sheen = 0.0f,
-			float sheen_tint = 0.5f,
-			float subsurface = 0.0f,
-			float clear_coat = 0.0f,
-			float clear_coat_gloss = 0.0f
+			float roughness					= 0.1f,
+			float specular					= 0.5f,
+			float metallic					= 0.0f,
+			float specular_tint				= 0.0f,
+			float sheen						= 0.0f,
+			float sheen_tint					= 0.5f,
+			float subsurface					= 0.0f,
+			float clear_coat					= 0.0f,
+			float clear_coat_gloss			= 0.0f
 		) : BSDF(ScatterType(BSDF_REFLECTION | BSDF_DIFFUSE | BSDF_GLOSSY), BSDFType::Disney, std::move(texture), std::move(normal)),
-			m_roughness(new ConstantTexture2D<float>(roughness)),
+			mp_roughness(new ConstantTexture2D<float>(roughness)),
 			m_specular(specular),
 			m_metallic(metallic),
 			m_specular_tint(specular_tint),
@@ -60,17 +60,17 @@ namespace Aya {
 			m_clear_coat(clear_coat),
 			m_clear_coat_gloss(clear_coat_gloss) {}
 		Disney(const char *file_tex,
-			float roughness = 0.1f,
-			float specular = 0.5f,
-			float metallic = 0.0f,
-			float specular_tint = 0.0f,
-			float sheen = 0.0f,
-			float sheen_tint = 0.5f,
-			float subsurface = 0.0f,
-			float clear_coat = 0.0f,
-			float clear_coat_gloss = 0.0f
+			float roughness					= 0.1f,
+			float specular					= 0.5f,
+			float metallic					= 0.0f,
+			float specular_tint				= 0.0f,
+			float sheen						= 0.0f,
+			float sheen_tint					= 0.5f,
+			float subsurface					= 0.0f,
+			float clear_coat					= 0.0f,
+			float clear_coat_gloss			= 0.0f
 		) : BSDF(ScatterType(BSDF_REFLECTION | BSDF_DIFFUSE | BSDF_GLOSSY), BSDFType::Disney, file_tex),
-			m_roughness(new ConstantTexture2D<float>(roughness)),
+			mp_roughness(new ConstantTexture2D<float>(roughness)),
 			m_specular(specular),
 			m_metallic(metallic),
 			m_specular_tint(specular_tint),
@@ -81,17 +81,17 @@ namespace Aya {
 			m_clear_coat_gloss(clear_coat_gloss) {}
 		Disney(const char *file_tex,
 			const char *file_normal,
-			float roughness = 0.1f,
-			float specular = 0.5f,
-			float metallic = 0.0f,
-			float specular_tint = 0.0f,
-			float sheen = 0.0f,
-			float sheen_tint = 0.5f,
-			float subsurface = 0.0f,
-			float clear_coat = 0.0f,
-			float clear_coat_gloss = 0.0f
+			float roughness					= 0.1f,
+			float specular					= 0.5f,
+			float metallic					= 0.0f,
+			float specular_tint				= 0.0f,
+			float sheen						= 0.0f,
+			float sheen_tint					= 0.5f,
+			float subsurface					= 0.0f,
+			float clear_coat					= 0.0f,
+			float clear_coat_gloss			= 0.0f
 		) : BSDF(ScatterType(BSDF_REFLECTION | BSDF_DIFFUSE | BSDF_GLOSSY), BSDFType::Disney, file_tex, file_normal),
-			m_roughness(new ConstantTexture2D<float>(roughness)),
+			mp_roughness(new ConstantTexture2D<float>(roughness)),
 			m_specular(specular),
 			m_metallic(metallic),
 			m_specular_tint(specular_tint),
@@ -103,24 +103,96 @@ namespace Aya {
 
 		Spectrum sample_f(const Vector3 &v_out, const Sample &sample,
 			const SurfaceIntersection &intersection, Vector3 *v_in, float *pdf, ScatterType types = BSDF_ALL, ScatterType *sample_types = nullptr) const override;
+		Spectrum f(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types = BSDF_ALL) const override;
 
+		Spectrum evaluate(const Vector3 &l_out, const Vector3 &l_in, const SurfaceIntersection &intersection, ScatterType types = BSDF_ALL) const;
 	private:
-		virtual float pdfInner(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types = BSDF_ALL) const override;
-	
+		float pdfInner(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types = BSDF_ALL) const override;
+		float evalInner(const Vector3 &v_out, const Vector3 &v_in, const SurfaceIntersection &intersection, ScatterType types = BSDF_ALL) const override;
+
+		float Fresnel_Schlick(const float cosd) const {
+			float R = 1.f - cosd;
+			float sqr_R = R * R;
+			return sqr_R * sqr_R * R;
+		}
 		float Fresnel_Schlick(const float cosd, const float reflectance) const {
-			float r = 1.f - cosd;
-			float sqr_r = r * r;
-			float fresnel = reflectance + (1.f - reflectance) * sqr_r * sqr_r * r;
+			float R = 1.f - cosd;
+			float sqr_R = R * R;
+			float fresnel = reflectance + (1.f - reflectance) * sqr_R * sqr_R * R;
 			float fresnel_conductor = fresnelConductor(cosd, .4f, 1.6f);
 
 			return Lerp(m_metallic, fresnel, fresnel_conductor);
 		}
 		float Fresnel_Schlick_Coat(const float cosd) const {
-			float r = 1.f - cosd;
-			float sqr_r = r * r;
-			float fresnel = .04f + (1.f - .04f) * sqr_r * sqr_r * r;
+			float R = 1.f - cosd;
+			float sqr_R = R * R;
+			float fresnel = .04f + (1.f - .04f) * sqr_R * sqr_R * R;
 			
 			return fresnel;
+		}
+
+		// Each Disney independent Term
+		float diffuseTerm(const Vector3 &l_out, const Vector3 &l_in, const float IdotH, const float roughness) const {
+			if (m_metallic == 1.f)
+				return 0.f;
+
+			// Diffuse fresnel - go from 1 at normal incidence to .5 at grazing
+			// and mix in diffuse retro-reflection based on roughness
+			float FL = Fresnel_Schlick(AbsCosTheta(l_in));
+			float FV = Fresnel_Schlick(AbsCosTheta(l_out));
+			float F_D90 = .5f + 2.f * IdotH * IdotH * roughness;
+
+			return (1.f + (F_D90 - 1.f) * FL) * (1.f + (F_D90 - 1.f) * FV) * float(M_1_PI);
+		}
+		float specularTerm(const Vector3 &l_out, const Vector3 &l_in, const Vector3 &wh,
+			const float OdotH, const float roughness, const float *fresnel = nullptr) const {
+			if (CosTheta(l_out) * CosTheta(l_in) <= 0.f)
+				return 0.f;
+
+			float Ds = GGX_D(wh, roughness * roughness);
+			if (Ds == 0.f)
+				return 0.f;
+
+			float normal_ref = Lerp(m_specular, .0f, .08f);
+			float Fs = fresnel ? *fresnel : Fresnel_Schlick(OdotH, normal_ref);
+
+			float rough_G = (.5f + .5f * roughness);
+			float Gs = GGX_G(l_out, l_in, wh, rough_G * rough_G);
+
+			return Fs * Ds * Gs / 
+				(4.f * AbsCosTheta(l_out) * AbsCosTheta(l_in));
+		}
+		float subsurfaceTerm(const Vector3 &l_out, const Vector3 &l_in, const float IdotH, const float roughness) const {
+			if (m_subsurface == 0.f)
+				return 0.f;
+			
+			float FL = Fresnel_Schlick(AbsCosTheta(l_in));
+			float FV = Fresnel_Schlick(AbsCosTheta(l_out));
+
+			// Based on Hanrahan-Krueger brdf approximation of isotropic bssrdf
+			// 1.25 scale is used to (roughly) preserve albedo
+			// Fss90 used to "flatten" retroreflection based on roughness
+			float Fss90 = IdotH * IdotH * roughness;
+			float Fss = (1.f + (Fss90 - 1.f) * FL) * (1.f + (Fss90 - 1.f) * FV);
+
+			return 1.25f * (Fss * (1.f / (AbsCosTheta(l_in) + AbsCosTheta(l_out)) - .5f) + .5f) * float(M_1_PI);
+		}
+		float clearCoatTerm(const Vector3 &l_out, const Vector3 &l_in, const Vector3 &wh,
+			const float IdotH, const float roughness) const {
+			if (m_clear_coat == 0.f)
+				return 0.f;
+
+			float rough = Lerp(roughness, .005f, .1f);
+
+			// clearcoat (ior = 1.5 -> F0 = 0.04)
+			float Dr = GGX_D(wh, rough);
+			if (Dr == 0.f)
+				return 0.f;
+			float Fr = Fresnel_Schlick_Coat(IdotH);
+			float Gr = GGX_G(l_out, l_in, wh, .25f);
+
+			return m_clear_coat * Fr * Dr * Gr /
+				(4.f * AbsCosTheta(l_out) * AbsCosTheta(l_in));
 		}
 	};
 }
