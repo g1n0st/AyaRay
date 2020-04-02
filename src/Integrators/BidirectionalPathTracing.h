d8359ffd507a14c5425f3a5ba4c5be439916371a
g1n0st <g1n0st@live.com>
2020-04-02 10:44:03 +0800

    Update Bidirectional Path Tracing pre data structure and functions declarations

---
+++ src/Integrators/BidirectionalPathTracing.h
@@ -0,0 +1,78 @@
+#ifndef AYA_BIDIRECTIONALINTEGRATORS_PATHTRACING_H
+#define AYA_BIDIRECTIONALINTEGRATORS_PATHTRACING_H
+
+#include "../Core/Integrator.h"
+
+namespace Aya {
+	class BidirectionalPathTracingIntegrator : public TiledIntegrator {
+	public:
+		struct PathState {
+			Point3 ori;
+			Vector3 dir;
+			Spectrum throughput;
+			uint32_t path_len;
+			
+			bool is_finite_light : 1;
+			bool specular_path : 1;
+
+			// Multiple Importance Sampling
+			// Implementing Vertex Connection and Merging
+			float dvcm;
+			float dvc;
+		};
+		struct PathVertex {
+			Spectrum throughput; // Path throughput including emission
+			uint32_t path_len;
+
+			// Diffuse surface Intersection local infomation
+			Intersection isect;
+			Vector3 v_in;
+
+			float dvcm;
+			float dvc;
+		};
+	private:
+		uint32_t m_max_depth;
+		const Camera *mp_cam;
+		Film *mp_film;
+
+	public:
+		BidirectionalPathTracingIntegrator(const TaskSynchronizer &task, const uint32_t &spp, uint32_t max_depth,
+			const Camera *camera, Film *film) :
+			TiledIntegrator(task, spp), m_max_depth(max_depth),
+			mp_cam(camera), mp_film(film) {
+		}
+		~BidirectionalPathTracingIntegrator() {
+		}
+
+		Spectrum li(const RayDifferential &ray, const Scene *scene, Sampler *sampler, RNG &rng, MemoryPool &memory) const override;
+
+		static PathState sampleLightSource(const Scene *scene, Sampler *sampler, RNG &rng);
+		static int generateLightPath(const Scene *scene, Sampler *sampler, RNG &rng,
+			const Camera *camera, Film *film, const int max_depth,
+			PathVertex *path, int *vertex_count,
+			const bool connect_to_cam = true, const int RR_depth = 3);
+		static Spectrum connectToCamera(const Scene *scene, Sampler *sampler, RNG &rng,
+			const Camera *camera, Film *film,
+			const Intersection &intersection, const PathVertex &path_vertex, Point3 *raster_pos);
+		static void sampleCamera(const Scene *scene,
+			const Camera *camera, Film *film,
+			const RayDifferential &ray, PathState &init_path);
+		static Spectrum connectToLight(const Scene *scene, Sampler *sampler, RNG &rng,
+			const RayDifferential &ray, const Intersection &intersection, PathState &cam_path);
+		static Spectrum hittingLightSource(const Scene *scene, RNG &rng,
+			const RayDifferential &ray, const Intersection &intersection, const Light *light, PathState &cam_path);
+
+		static Spectrum connectVertex(const Scene *scene, RNG &rng,
+			const Intersection &intersection, const PathVertex &light_vertex, const PathState &cam_path);
+		static bool sampleScattering(const Scene *scene, RNG &rng,
+			const RayDifferential &ray, const Intersection &intersection, const Sample& bsdf_sample, const PathState &cam_path);
+
+		inline static float MIS(const float val) {
+			// Power Heuristic Method
+			return val * val;
+		}
+	};
+}
+
+#endif
